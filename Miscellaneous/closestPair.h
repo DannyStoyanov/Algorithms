e1fb7b178024366b1ed5e0a4d22b24eeffc098e0
Daniel Stoyanov <danielstoyanov@Daniels-MacBook-Air.local>
2022-05-16 14:30:04 +0300

    add closestPair

---
+++ Miscellaneous/closestPair.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "mergeSort.h"
+
+std::pair<int, int> closestPair(int arr[], std::size_t size) {
+    if(size == 0) {
+        return std::make_pair(INT_MIN, INT_MAX);
+    }
+    if(size == 1) {
+        return std::make_pair(arr[0], INT_MAX);
+    }
+    if(size == 2) {
+        return std::make_pair(arr[0], arr[1]);
+    }
+    mergeSort<int>(arr, size);
+    int closest = INT_MAX;
+    int first = arr[0];
+    int second = arr[1];
+    for (size_t i = 0; i < size-1; i++) {
+        if(arr[i+1] - arr[i] < closest) {
+            closest = arr[i+1] - arr[i];
+            first = arr[i];
+            second = arr[i+1];
+        }
+    }
+    return std::make_pair(first, second);
+}
