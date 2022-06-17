e7cf5be31bac84be92c0cbefa1e46228f7cefa47
Daniel Stoyanov <danielstoyanov@Daniels-MacBook-Air.local>
2022-06-17 14:56:19 +0300

    add 2SUM problem

---
+++ Miscellaneous/2SUM.h
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Array must be sorted before using!
+
+bool twoSum(int* arr, int size, int sum) {
+    int i=0;
+    int j=size-1;
+    while(i < j) {
+        if((arr[i] + arr[j]) == sum) {
+            return true;
+        }
+        else if (arr[i] + arr[j] < sum) {
+            i++;
+        }
+        else {
+            j--;
+        }
+    }
+    return false;
+}
