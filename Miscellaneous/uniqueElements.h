f962dc1af0f3962facb5958bffec1531f59901c8
Daniel Stoyanov <danielstoyanov@Daniels-MacBook-Air.local>
2022-06-17 14:17:12 +0300

    add uniqueElements

---
+++ Miscellaneous/uniqueElements.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "mergeSort.h"
+
+template <typename T>
+bool uniqueElements(T* arr, std::size_t size) {
+    mergeSort<T>(arr, size);
+    for (std::size_t i = 1; i < size; i++) {
+        if(arr[i] == arr[i-1]) {
+            return false;
+        }
+    }
+    return true;
+}
