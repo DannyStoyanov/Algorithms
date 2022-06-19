b5895c10ede3552d1f8ac4b070262ca610fbdba2
Daniel Stoyanov <daniel.stoyanov1999@gmail.com>
2022-06-19 21:59:32 +0300

    add Khan's Toplogical Sorting algorithm

--- Graph algorithms/Graph.hpp
+++ Graph algorithms/Graph.hpp
@@ -131,6 +131,69 @@ std::vector<size_t>  Graph::TopoSort_Tarjan() const {
     return result;
 }
 
+
+
+// Need for testing:
+void printVector(std::vector<size_t> v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        std::cout<<v[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
+
+bool contains(std::vector<size_t>& vec, size_t x) {
+    for(std::vector<size_t>::iterator it = vec.begin(); it != vec.end(); it++) {
+        if(*it == x) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void addToSet(std::vector<size_t>& set, size_t x) {
+    if(!contains(set, x)) {
+        set.push_back(x);
+    }
+}
+
 std::vector<size_t> Graph::TopoSort_Kahn() const {
-    
+    std::vector<size_t> set;
+    std::vector<Color> color(V);
+    std::vector<size_t> arr(V, 0);
+    std::vector<size_t> result(V);
+    for (size_t u = 0; u < adj.size(); u++) {
+        for (size_t v = 0; v < adj[u].size(); v++) {
+            size_t neighbour = adj[u][v];
+            arr[neighbour]++;
+        }
+    }
+    for(size_t currentVertex=0; currentVertex < adj.size(); currentVertex++) {
+        if(arr[currentVertex] == 0) {
+            addToSet(set, currentVertex);
+            color[currentVertex] = gray;
+        }
+        else {
+            color[currentVertex] = white;
+        }
+    }
+    size_t k=0;
+    while(!set.empty()) {
+        size_t currentVertex = set.back();
+        set.pop_back();
+        result[k]=currentVertex;
+        k++;
+        for(size_t i=0; i < adj[currentVertex].size(); i++) {
+            size_t neighbour = adj[currentVertex][i];
+            arr[neighbour]--;
+            if(arr[neighbour] == 0) {
+                addToSet(set, neighbour);
+                color[neighbour] = gray;
+            }
+        }
+        color[currentVertex] = black;
+    }
+    if(k<V) {
+        return std::vector<size_t>(0);
+    }
+    return result;
 }
--- Graph algorithms/readme.md
+++ Graph algorithms/readme.md
@@ -5,3 +5,4 @@
 - Breadth First Search - BFS
 - Depth First Search - DFS
 - Tarjan's Topological Sorting
+- Kahn's Topological Sorting
