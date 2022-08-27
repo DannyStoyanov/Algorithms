9f00305318c0d394d724df4fcab61f972ddfe202
Daniel Stoyanov <daniel.stoyanov1999@gmail.com>
2022-08-27 13:26:46 +0300

    add predominantElement second solution with linear time complexity

--- Miscellaneous/predominantElement.h
+++ Miscellaneous/predominantElement.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include "mergeSort.h"
 
 // You are given an array of integers of size N. 
@@ -6,7 +7,22 @@
 // A dominant number in an array is an integer that occurs
 // more than N/2 times in the array, where N is the array’s length.
 
-int predominantElement(int arr[], std::size_t size) {
+// Time complexity: O(nlgn)
+int predominantElementV2(int arr[], std::size_t size) {
     mergeSort(arr, size);
     return arr[size/2];
+}
+
+// Time complexity: O(n)
+int predominantElement(int arr[], std::size_t size) {
+    std::stack<int> st;
+    for(size_t i=0; i<size; i++) {
+        if(st.empty() || arr[i] == st.top()) {
+            st.push(arr[i]);
+        }
+        else {
+            st.pop();
+        }
+    }
+    return st.top();
 }
